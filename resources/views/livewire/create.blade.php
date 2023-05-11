90bee638f055a976409d5e3643eed5290abaea83
Hari Sugirthan <hsugeen14@gmail.com>
2023-05-11 17:26:40 +0530

    .defer modifier added

--- resources/views/livewire/create.blade.php
+++ resources/views/livewire/create.blade.php
@@ -1,12 +1,12 @@
 <form>
     <div class="form-group mb-3">
         <label for="categoryName">Name:</label>
-        <input type="text" class="form-control @error('name') is-invalid @enderror" id="categoryName" placeholder="Enter Name" wire:model="name">
+        <input type="text" class="form-control @error('name') is-invalid @enderror" id="categoryName" placeholder="Enter Name" wire:model.defer="name">
         @error('name') <span class="text-danger">{{ $message }}</span>@enderror
     </div>
     <div class="form-group mb-3">
         <label for="categoryDescription">Description:</label>
-        <textarea class="form-control @error('description') is-invalid @enderror" id="categoryDescription" wire:model="description" placeholder="Enter Description"></textarea>
+        <textarea class="form-control @error('description') is-invalid @enderror" id="categoryDescription" wire:model.defer="description" placeholder="Enter Description"></textarea>
         @error('description') <span class="text-danger">{{ $message }}</span>@enderror
     </div>
     <div class="d-grid gap-2">
